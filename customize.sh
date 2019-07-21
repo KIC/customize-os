ec0ad881e8eb19bfbf9d4dbdc2a6788f61e39850
kic <ch9.ki7@gmail.com>
2019-07-21 15:31:54 +0200

    add xmodmap to bashrc

--- customize.sh
+++ customize.sh
@@ -36,6 +36,7 @@ echo "keycode 117=End" >>  ~/.Xmodmap
 echo "keycode 110=Prior" >>  ~/.Xmodmap
 echo "keycode 115=Next" >>  ~/.Xmodmap
 sudo bash -c 'echo "sleep 2 && xmodmap ~/.Xmodmap" > /etc/X11/Xsession.d/99xmodmap'
+sudo bash -c 'xmodmap ~/.Xmodmap" >> ~/.bashrc'
 
 # make python3 the default
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.7 0
