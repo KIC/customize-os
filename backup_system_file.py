9541764a66bb2b1e1c34d850dca82751a4eaf824
kic <kic@kic.kic>
2021-02-01 13:33:49 +0000

    add some scripts

--- backup_system_file.py
+++ backup_system_file.py
@@ -1,13 +1,31 @@
 #!/usr/bin/python3
 import os
 import sys
-from shutil import copyfile, copytree
+from shutil import copytree, copyfile
 
 if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        raise ValueError("provide source dest files")
+
     args = sys.argv[1:-1]
-    target = sys.argv[-1]
+    target = os.path.realpath(sys.argv[-1])
+    print(f'copy from {args} to {target}')
 
     for arg in args:
-        base_path = os.path.split(arg)[:-1] if os.path.isdir(arg) else os.path.dirname(arg)
-        os.makedirs(base_path, exist_ok=True)
-        copytree
+        base_path = os.path.join(*os.path.split(arg)[:-1]) if os.path.isdir(arg) else os.path.dirname(arg)
+        target_path = os.path.join(target, base_path[1:])
+
+        print(f'mkdir {target_path}')
+        os.makedirs(target_path, exist_ok=True)
+
+        target_file = os.path.join(target, arg[1:])
+        print(f'copy {arg} to {target_file}')
+        if os.path.isdir(arg):
+            copytree(arg, target_file, dirs_exist_ok=True)
+        else:
+            copyfile(arg, target_file)
+
+"""
+backup_system_file.py /etc/aliases /etc/apt /etc/cron* /etc/environment* /etc/fstab /etc/hosts /etc/ly /etc/profile* \
+ /etc/ranger /etc/default/grub* /etc/default/grub backup
+"""
